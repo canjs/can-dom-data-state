fdbec784c67cc92e42a0623ad39f5e0108cd6351
Bradley Momberger <brad@bitovi.com>
2019-04-23 18:55:40 -0400

    Use the can-dom-data library instead of duplicating its functionality

--- can-dom-data-state.js
+++ can-dom-data-state.js
@@ -1,64 +1,18 @@
 'use strict';
 var namespace = require('can-namespace');
-var domMutate = require('can-dom-mutate');
 var CID = require("can-cid");
+var domData = require("can-dom-data");
 
-var isEmptyObject = function(obj){
-	/* jshint -W098 */
-	for(var prop in obj) {
-		return false;
-	}
-	return true;
-};
+var slice = [].slice;
+function unwrap(obj, key) {
+    return function() {
+	var args = slice.call(arguments, 0);
+	args.unshift(this);
+	return obj[key].apply(obj, args);
+    };
+}
 
-var data = {};
-var removedDisposalMap = {};
-
-// delete this node's `data`
-// returns true if the node was deleted.
-var deleteNode = function() {
-	var id = CID.get(this);
-	var nodeDeleted = false;
-	if(id && data[id]) {
-		nodeDeleted = true;
-		delete data[id];
-	}
-	if (removedDisposalMap[id]) {
-		removedDisposalMap[id]();
-		delete removedDisposalMap[id];
-	}
-	return nodeDeleted;
-};
-
-var setData = function(name, value) {
-	var id = CID(this);
-	var store = data[id] || (data[id] = {});
-	if (name !== undefined) {
-		store[name] = value;
-		var isNode = !!(this && typeof this.nodeType === 'number');
-		if (isNode && !removedDisposalMap[id]) {
-			var target = this;
-			removedDisposalMap[id] = domMutate.onNodeRemoval(target, function () {
-				var doc = target.ownerDocument;
-				var ownerNode = doc.contains ? doc : doc.documentElement;
-				if (!ownerNode || !ownerNode.contains(target)) {
-					setTimeout(function () {
-						deleteNode.call(target);
-					}, 13);
-				}
-			});
-		}
-	}
-	return store;
-};
-
-/*
- * Core of domData that does not depend on mutationDocument
- * This is separated in order to prevent circular dependencies
- */
 var domDataState = {
-	_data: data,
-	_removalDisposalMap: removedDisposalMap,
 
 	getCid: function() {
 		// TODO log warning! to use can-cid directly
@@ -72,26 +26,11 @@ var domDataState = {
 
 	expando: CID.domExpando,
 
-	get: function(key) {
-		var id = CID.get(this),
-			store = id && data[id];
-		return key === undefined ? store : store && store[key];
-	},
-
-	set: setData,
-
-	clean: function(prop) {
-		var id = CID.get(this);
-		var itemData = data[id];
-		if (itemData && itemData[prop]) {
-			delete itemData[prop];
-		}
-		if(isEmptyObject(itemData)) {
-			deleteNode.call(this);
-		}
-	},
-
-	delete: deleteNode
+	_data: domData._data,
+	get: unwrap(domData, "get"),
+	set: unwrap(domData, "set"),
+	clean: unwrap(domData, "clean"),
+	delete: unwrap(domData, "delete"),
 };
 
 if (namespace.domDataState) {
